4ecf42d58921362248a7f5a8dc8327c36636c850
Khadaassi <aassikhadija@yahoo.fr>
2025-02-28 15:40:58 +0100

    change the front of loan form

--- Bamk/loan/forms.py
+++ Bamk/loan/forms.py
@@ -1,58 +1,85 @@
-# loan/forms.py
-from django import forms
-
-# class LoanRequestForm(forms.Form):
-#     """Formulaire pour soumettre une demande de prêt"""
-#     State = forms.CharField(
-#         label="État",
-#         max_length=50,
-#         widget=forms.TextInput(attrs={'class': 'form-control'})
-#     )
-#     NAICS = forms.IntegerField(
-#         label="Code NAICS",
-#         widget=forms.NumberInput(attrs={'class': 'form-control'})
-#     )
-#     NewExist = forms.ChoiceField(
-#         label="Type d'entreprise",
-#         choices=[(1, "Nouvelle entreprise"), (2, "Entreprise existante")],
-#         widget=forms.Select(attrs={'class': 'form-control'})
-#     )
-#     RetainedJob = forms.IntegerField(
-#         label="Emplois conservés",
-#         widget=forms.NumberInput(attrs={'class': 'form-control'})
-#     )
-#     FranchiseCode = forms.IntegerField(
-#         label="Code franchise",
-#         widget=forms.NumberInput(attrs={'class': 'form-control'})
-#     )
-#     UrbanRural = forms.ChoiceField(
-#         label="Zone",
-#         choices=[(1, "Urbain"), (2, "Rural"), (0, "Non défini")],
-#         widget=forms.Select(attrs={'class': 'form-control'})
-#     )
-#     GrAppv = forms.FloatField(
-#         label="Montant du prêt ($)",
-#         widget=forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'})
-#     )
-#     Bank = forms.CharField(
-#         label="Banque",
-#         max_length=100,
-#         widget=forms.TextInput(attrs={'class': 'form-control'})
-#     )
-#     Term = forms.IntegerField(
-#         label="Durée (mois)",
-#         widget=forms.NumberInput(attrs={'class': 'form-control'})
-#     )
-#     class Meta:
-#         fields = ['State', 'NAICS', 'NewExist', 'RetainedJob', 'FranchiseCode', 'UrbanRural', 'GrAppv', 'Bank', 'Term']
-    
-#     def save(self, commit=True):
-#         pass
 from django import forms
 from .models import LoanRequest
 
 class LoanRequestForm(forms.ModelForm):
+
+    state = forms.CharField(
+        label="State",
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: California'
+        })
+    )
+    
+    naics = forms.IntegerField(
+        label="NAICS code",
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: 541330'
+        })
+    )
+
+    new_exist = forms.ChoiceField(
+        label="New or existing business",
+        choices=[(1, "Nouvelle entreprise"), (2, "Entreprise existante")],
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    retained_job = forms.IntegerField(
+        label="Retained jobs",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: 5'
+        })
+    )
+
+    franchise_code = forms.IntegerField(
+        label="Franchise Code (0 if not a franchise)",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: 1234'
+        })
+    )
+
+    urban_rural = forms.ChoiceField(
+        label="Zone (urban or rural)",
+        choices=[(1, "Urban"), (2, "Rural"), (0, "Non specified")],
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    gr_appv = forms.FloatField(
+        label="Approved amount",
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: 50000.00',
+            'step': '0.01'
+        })
+    )
+
+    bank = forms.CharField(
+        label="Bank",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: Bank of America'
+        })
+    )
+
+    term = forms.IntegerField(
+        label="Term (months)",
+        min_value=1,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ex: 36'
+        })
+    )
+
     class Meta:
         model = LoanRequest
         fields = ['state', 'naics', 'new_exist', 'retained_job', 'franchise_code', 
                   'urban_rural', 'gr_appv', 'bank', 'term']
+    

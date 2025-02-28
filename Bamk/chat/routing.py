d72c0747a5292bef74d8b6f03235d2b18a738bf0
Khadaassi <aassikhadija@yahoo.fr>
2025-02-28 09:59:18 +0100

    notification in chat

--- Bamk/chat/routing.py
+++ Bamk/chat/routing.py
@@ -5,9 +5,10 @@
 #     re_path(r"ws/chat/$", ChatConsumer.as_asgi()),
 # ]
 from django.urls import re_path
-from .consumers import ClientChatConsumer, AdvisorChatConsumer
+from .consumers import ClientChatConsumer, AdvisorChatConsumer, NotificationConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/advisor/(?P<client_id>\d+)/$", AdvisorChatConsumer.as_asgi()),  # 🔹 Chat privé pour chaque client
     re_path(r"ws/chat/client/$", ClientChatConsumer.as_asgi()),  # 🔹 Chat unique du client vers son conseiller
+    re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),
 ]
